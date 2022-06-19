68d1eab051d233a9709efb15458af2600ea7cb35
weklihua <wek.lihua@gmail.com>
2022-06-19 12:16:00 -0700

    initial commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/flights')
+
+const db = mongoose.connection
+
+db.on('connected', function(){
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
+})
---
+++ models/flight.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN'
+    },
+    flightNo: {
+        type: Number,
+        required: true,
+        min: 10,
+        max: 9999,
+
+    },
+    departs: {
+        type: Date,
+        default: function(){
+            return new Date().getFullYear() + 1
+        }
+    }
+})
+
+module.exports = mongoose.model('Flight', flightSchema)
